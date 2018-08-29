000d5d599533ba73599bca4aeb7631afeae9ae01
Guy Perkal <guy@bigpanda.io>
2018-08-29 20:59:45 +0300

    Show a scrollable comics grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { debounce } from 'lodash';
 import { getComics } from './api/xkcd';
 import './App.css';
 
 class App extends Component {
-  state = {
-    comics: []
+  constructor() {
+    super();
+    this.state = {
+      nextLoadStartIndex: 1,
+      comics: [],
+    };
+    this.trackScrolling = debounce(this.trackScrolling.bind(this), 200);
   }
 
   async componentDidMount() {
-    const comics = await getComics(1, 10);
+    document.addEventListener('scroll', this.trackScrolling);
+    const comics = await getComics(1, 11);
     this.setState({
       comics,
+      nextLoadStartIndex: 11,
     });
   }
+  
+  componentWillUnmount() {
+    document.removeEventListener('scroll', this.trackScrolling);
+  }
+  
+  async trackScrolling() {
+    const { nextLoadStartIndex: startIndex, comics: currComics } = this.state;
+    const el = document.getElementById('app');
+
+    if (Math.floor(el.getBoundingClientRect().bottom) <= window.innerHeight) {
+      console.log('header bottom reached');
+      const comics = await getComics(startIndex, startIndex + 10);
+      this.setState({
+        comics: currComics.concat(comics),
+        nextLoadStartIndex: startIndex + 10,
+      });
+    }
+  };
 
   render() {
     const { comics } = this.state;
 
     return (
-      <div className="app">
+      <div id="app" className="app">
+        <div className="header">
+          <h1>{`Loaded ${comics.length} comics!`}</h1>
+        </div>
         <div className="comics">
-          {comics.map(comic => <img src={comic.data.img} alt={comic.data.alt} />)}
+          {comics.map(comic => {
+            return (
+              <img src={comic.data.img} alt={comic.data.alt} />
+            );
+          })}
         </div>
       </div>
     );
